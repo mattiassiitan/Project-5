907c2a13ac1ea49bf9b2322f596a3b5c7c06960a
mattiassiitan <mattias.siitan@gmail.com>
2023-08-03 21:11:01 +0300

    done

--- cypress/e2e/issue-time-tracking.cy.js
+++ cypress/e2e/issue-time-tracking.cy.js
@@ -1,9 +1,9 @@
 
-const testname = 'TEST_TIME_TRACKING';
-const first_estimate = '10'
-const edit_estimate = '20'
-const time_spent = '2'
-const time_remaining = '5'
+const testName = 'TEST_TIME_TRACKING';
+const firstEstimate = '10'
+const editEstimate = '20'
+const timeSpent = '2'
+const timeRemaining = '5'
 
 describe('Issue time tracking', () => {
     beforeEach(() => {
@@ -18,46 +18,46 @@ describe('Issue time tracking', () => {
         //Create issue
         cy.get('[data-testid="modal:issue-create"]').within(() => {
             cy.get('.ql-editor').type('Test time tracking 10 hours')
-            cy.get('input[name="title"]').type(testname)
+            cy.get('input[name="title"]').type(testName)
             cy.get('button[type="submit"]').debounced('click');
         });
         cy.get('[data-testid="modal:issue-create"]').should('not.exist');
         cy.contains('Issue has been successfully created.').should('be.visible');
         cy.reload();
         cy.contains('Issue has been successfully created.').should('not.exist');
-        cy.contains(testname).debounced('click');
+        cy.contains(testName).debounced('click');
 
         //Check that time estiomation is 0 and change it to 10h, assert
         cy.get('input[placeholder="Number"]').should('be.visible').should('be.empty').should('have.value', '');
         cy.contains('No time logged').should('be.visible')
         cy.get('input[placeholder="Number"]')
-        cy.get('input[placeholder="Number"]').debounced('type', first_estimate);
-        cy.contains(`${first_estimate}h estimated`).should('be.visible')
+        cy.get('input[placeholder="Number"]').debounced('type', firstEstimate);
+        cy.contains(`${firstEstimate}h estimated`).should('be.visible')
         cy.get('[data-testid="icon:close"]').click()
         //assert
-        cy.contains(testname).click();
-        cy.get('input[placeholder="Number"]').should('have.value', first_estimate);
-        cy.contains(`${first_estimate}h estimated`).should('be.visible')
+        cy.contains(testName).click();
+        cy.get('input[placeholder="Number"]').should('have.value', firstEstimate);
+        cy.contains(`${firstEstimate}h estimated`).should('be.visible')
 
         //Edit time estimation to 20, assert
-        cy.get('input[placeholder="Number"]').clear().debounced('type', edit_estimate);
-        cy.contains(`${edit_estimate}h estimated`).should('be.visible')
-        cy.contains(`${first_estimate}h estimated`).should('not.exist')
+        cy.get('input[placeholder="Number"]').clear().debounced('type', editEstimate);
+        cy.contains(`${editEstimate}h estimated`).should('be.visible')
+        cy.contains(`${firstEstimate}h estimated`).should('not.exist')
         cy.get('[data-testid="icon:close"]').click()
         //assert
-        cy.contains(testname).click();
-        cy.get('input[placeholder="Number"]').should('have.value', edit_estimate);
-        cy.contains(`${edit_estimate}h estimated`).should('be.visible')
-        cy.contains(`${first_estimate}h estimated`).should('not.exist')
+        cy.contains(testName).click();
+        cy.get('input[placeholder="Number"]').should('have.value', editEstimate);
+        cy.contains(`${editEstimate}h estimated`).should('be.visible')
+        cy.contains(`${firstEstimate}h estimated`).should('not.exist')
 
         //Remove time estimation
         cy.get('input[placeholder="Number"]').debounced('clear')
-        cy.contains(`${edit_estimate}h estimated`).should('not.exist')
+        cy.contains(`${editEstimate}h estimated`).should('not.exist')
         cy.get('[data-testid="icon:close"]').click()
         //assert
-        cy.contains(testname).click();
+        cy.contains(testName).click();
         cy.get('input[placeholder="Number"]').should('be.visible').should('be.empty').should('have.value', '');
-        cy.contains(`${edit_estimate}h estimated`).should('not.exist')
+        cy.contains(`${editEstimate}h estimated`).should('not.exist')
         //SYSTEM HAS A BUG - 20h estimation reappeares!!!
     });
 
@@ -66,36 +66,36 @@ describe('Issue time tracking', () => {
         //Create issue
         cy.get('[data-testid="modal:issue-create"]').within(() => {
             cy.get('.ql-editor').type('Test time logging')
-            cy.get('input[name="title"]').type(testname)
+            cy.get('input[name="title"]').type(testName)
             cy.get('button[type="submit"]').debounced('click');
         });
         cy.get('[data-testid="modal:issue-create"]').should('not.exist');
         cy.contains('Issue has been successfully created.').should('be.visible');
         cy.reload();
         cy.contains('Issue has been successfully created.').should('not.exist');
-        cy.contains(testname).debounced('click');
+        cy.contains(testName).debounced('click');
 
         //Check that logged time is 0 and change it, assert
         cy.contains('No time logged').should('be.visible')
         cy.get('[data-testid="icon:stopwatch"]').click()
         cy.get('[data-testid="modal:tracking"]').should('be.visible').within(() => {
-            cy.get('input[placeholder="Number"]').first().type(time_spent)
-            cy.get('input[placeholder="Number"]').last().type(time_remaining)
+            cy.get('input[placeholder="Number"]').first().type(timeSpent)
+            cy.get('input[placeholder="Number"]').last().type(timeRemaining)
             cy.contains('Done').debounced('click')
         });
         //assert
         cy.contains('No time logged').should('not.exist')
-        cy.contains(`${time_spent}h logged`).should('be.visible')
-        cy.contains(`${time_remaining}h remaining`).should('be.visible')
+        cy.contains(`${timeSpent}h logged`).should('be.visible')
+        cy.contains(`${timeRemaining}h remaining`).should('be.visible')
         cy.get('[data-testid="icon:close"]').click()
 
-        cy.contains(testname).click();
+        cy.contains(testName).click();
         cy.contains('No time logged').should('not.exist')
-        cy.contains(`${time_spent}h logged`).should('be.visible')
-        cy.contains(`${time_remaining}h remaining`).should('be.visible')
+        cy.contains(`${timeSpent}h logged`).should('be.visible')
+        cy.contains(`${timeRemaining}h remaining`).should('be.visible')
 
         //Remove logged time, assert
-        cy.contains(`${time_spent}h logged`).should('be.visible')
+        cy.contains(`${timeSpent}h logged`).should('be.visible')
         cy.get('[data-testid="icon:stopwatch"]').click()
         cy.get('[data-testid="modal:tracking"]').within(() => {
             cy.get('input[placeholder="Number"]').first().clear()
@@ -103,14 +103,14 @@ describe('Issue time tracking', () => {
             cy.contains('Done').debounced('click')
         });
         cy.contains('No time logged').should('be.visible')
-        cy.contains(`${time_spent}h logged`).should('not.exist')
-        cy.contains(`${time_remaining}h remaining`).should('not.exist')
+        cy.contains(`${timeSpent}h logged`).should('not.exist')
+        cy.contains(`${timeRemaining}h remaining`).should('not.exist')
         cy.get('[data-testid="icon:close"]').click()
         //assert
-        cy.contains(testname).click();
+        cy.contains(testName).click();
         cy.contains('No time logged').should('be.visible')
-        cy.contains(`${time_spent}h logged`).should('not.exist')
-        cy.contains(`${time_remaining}h remaining`).should('not.exist')
+        cy.contains(`${timeSpent}h logged`).should('not.exist')
+        cy.contains(`${timeRemaining}h remaining`).should('not.exist')
 
     });
 
